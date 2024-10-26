e396231d5aadc28ccdb7cb023805a34503990bd3
Vinicius Melo <viniiciiusmelo@gmail.com>
2024-10-26 20:39:08 -0300

    Adicionar arquivos de projeto.

---
+++ AgroSeller/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgroSeller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
